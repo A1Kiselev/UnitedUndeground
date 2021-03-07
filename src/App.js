7c0410c37e4ca486f21b6dac4c30093e497e851a
Kiselyov <a6.kiselyov@itransition.com>
2021-03-07 14:18:48 +0300

    add music

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
+import ReactAudioPlayer from 'react-audio-player';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src='https://image.flaticon.com/icons/png/512/86/86508.png' className="App-logo" alt="logo" />
+        <img src='https://image.flaticon.com/icons/png/512/86/86508.png' className="App-logo" alt="logo"/>
+
+        <ReactAudioPlayer
+          src="/Марко Поло - Звёздные капитаны.mp3"
+          controls
+        />
+
         <p className='greeting'>
           Вы вошли на земли клана United Underground
         </p>
         <p className='description'>
-         <span> Гости и бойцы рангом ниже Офицеров ниспособны пока осознать все великолепие увиденного.</span>
+          <span> Гости и бойцы рангом ниже Офицеров ниспособны пока осознать все великолепие увиденного.</span>
           <br/>
-         <span> В ближайшее время заручившись поддержкой наших союзников Мы расширим свое влияние и в тот момент даже нижшим
+          <span> В ближайшее время заручившись поддержкой наших союзников Мы расширим свое влияние и в тот момент даже нижшим
           простолюдинам будет дарована возможность узреть величие Клана.
           </span>
         </p>
