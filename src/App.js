48d750b8b562d6e55b7b55ad242e3dd4c5a7169d
Kiselyov <a6.kiselyov@itransition.com>
2021-03-07 13:38:42 +0300

    Init United Underground

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src='https://image.flaticon.com/icons/png/512/86/86508.png' className="App-logo" alt="logo" />
+        <p className='greeting'>
+          Вы вошли на земли клана United Underground
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className='description'>
+         <span> Гости и бойцы рангом ниже Офицеров ниспособны пока осознать все великолепие увиденного.</span>
+          <br/>
+         <span> В ближайшее время заручившись поддержкой наших союзников Мы расширим свое влияние и в тот момент даже нижшим
+          простолюдинам будет дарована возможность узреть величие Клана.
+          </span>
+        </p>
+        <p>
+          <span className='help'>
+            нужно что-то придусать, а то мне в лом((
+          </span>
+        </p>
       </header>
     </div>
   );
